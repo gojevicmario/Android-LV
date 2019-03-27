241b196cf7d3538e38fbe48530de9b133de27834
gojevic.mario <gojevic.mario@gmail.com>
2019-03-27 21:55:55 +0100

    made a shit layout

--- app/src/main/java/com/example/lv2/activities/MainActivity.java
+++ app/src/main/java/com/example/lv2/activities/MainActivity.java
@@ -1,13 +1,11 @@
-package com.example.lv2.activites;
+package com.example.lv2.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Toast;
 import android.widget.Spinner;
 
 import com.example.lv2.R;
--- app/src/main/java/com/example/lv2/activities/WeatherDetailsActivity.java
+++ app/src/main/java/com/example/lv2/activities/WeatherDetailsActivity.java
@@ -1,8 +1,7 @@
-package com.example.lv2.activites;
+package com.example.lv2.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.lv2.R;
